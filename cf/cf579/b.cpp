bc5abf572e7ca2b6e23415439bedba492848bcc6
pediogo <pedrodiogo219@gmail.com>
2019-11-11 21:23:03 -0200

    solving B cf579

--- cf/cf579/b.cpp
+++ cf/cf579/b.cpp
@@ -1,46 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define INF 100000
 
-int vis[1100];
-int vet[10000], dist[1000];
+int vet[1000];
 int main(){
   int t; scanf("%d", &t);
   while(t--){
-
-    int n, me = INF, ma = -INF;
-    scanf("%d", &n);
-    for(int i = 0; i < 4*n; i++){
+    int n; scanf("%d", &n);
+    for(int i=0; i<n*4; i++){
       scanf("%d", &vet[i]);
-      me = min(me, vet[i]);
-      ma = max(ma, vet[i]);
-      vis[vet[i]]++;
     }
 
     sort(vet, vet+(4*n));
-    int pos = 0;
-    dist[pos++] = vet[0];
-    for(int i = 1; i < 4*n; i++){
-      if(vet[i] != vet[i-1]){
-        dist[pos++] = vet[i];
-      }
+
+    n*=4;
+    int valor = 0;
+    if(vet[0] != vet[1] || vet[n-1] != vet[n-2]){
+      puts("NO");
+      continue;
     }
 
-    int x = me*ma;
-
-
-    bool val = true;
-    for(int i = 0; i < pos; i++){
-      if(vis[vet[i]]  == 0) continue;
-      if( vis[vet[i]] % 2 != 0 || x % vet[i] != 0 || vis[(x/vet[i]) ] < vis[vet[i]]){
-        val = false;
+    valor = vet[0] * vet[n-1];
+    // printf("valor: %d\n", valor);
+    bool flag = false;
+    for(int i = 0; i < n/4; i++){
+      if(vet[2*i]!=vet[2*i+1] || vet[n - 2*i - 1] != vet[n - 2*i - 2] || vet[2*i]*vet[n - 2*i - 1] != valor){
+        puts("NO");
+        flag = true;
         break;
       }
-
-      vis[ (x/vet[i]) ] -= vis[vet[i]];
-      vis[vet[i]] = 0;
     }
-    if(val) printf("YES\n");
-    else printf("NO\n");
+
+    if(!flag){
+      puts("YES");
+    }
+
   }
 }
